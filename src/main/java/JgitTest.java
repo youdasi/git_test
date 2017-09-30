6b57d8eb5593ab865938bfa71f79e0b6e9cbde6c
陈东 <chendong@cmiot.chinamobile.com>
2017-09-30 21:14:28 +0800

    打印日志

--- src/main/java/JgitTest.java
+++ src/main/java/JgitTest.java
@@ -1,5 +1,33 @@
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.LogCommand;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+
+import java.io.File;
+import java.util.Iterator;
+
 public class JgitTest {
 
     public static void main(String[] args) {
+        FileRepositoryBuilder builder = new FileRepositoryBuilder();
+        File gitFile = new File(".git");
+        try (Repository repository = builder.setGitDir(gitFile)
+                .readEnvironment()
+                .findGitDir()
+                .build()) {
+            Git git = new Git(repository);
+            LogCommand logCommand = git.log();
+            Iterable<RevCommit> revCommits = logCommand.call();
+            Iterator<RevCommit> iterator = revCommits.iterator();
+            while (iterator.hasNext()) {
+                RevCommit revCommit = iterator.next();
+                String meg = revCommit.getFullMessage();
+                System.out.println(meg);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
